f82195247b5556439a95595ac84098bbcf3f5c71
jurilents <keeeneeek@gmail.com>
2021-12-09 02:21:53 +0200

    server v2

--- src/ElementarySchool.Web/Controllers/HomeController.cs
+++ src/ElementarySchool.Web/Controllers/HomeController.cs
@@ -17,7 +17,7 @@ namespace ElementarySchool.Controllers
 		[HttpGet]
 		public ActionResult Get()
 		{
-			return this.Ok(new { Success = true, Server = "V_1" });
+			return this.Ok(new { Success = true, Server = "V_2" });
 		}
 	}
 }
